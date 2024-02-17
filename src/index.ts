3b7b97e925d09b5a22283297a26e82726b85fbf0
chadjaa-sofiane <sofianne012@gmail.com>
2024-02-17 23:02:40 +0100

    Setup redis Client to use.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("something something");
+import redisClient from "./redis/redisClient";
+import createGPTChat from "./services/chatGpt";
+import completion from "./services/chatGpt";
+
+const main = async () => {
+  const chat = createGPTChat();
+  const message = await chat.getMessage({
+    userId: "1",
+    message:
+      "I want to look for a job for these technologies: HTML, CSS, JS, Node, React",
+  });
+  console.log(message);
+  try {
+    const data = await redisClient.keys("*");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
