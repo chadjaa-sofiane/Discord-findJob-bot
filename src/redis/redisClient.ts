517a0378c06081795d306f5f52597adb479d1db8
chadjaa-sofiane <sofianne012@gmail.com>
2024-02-22 15:40:33 +0100

    Create the redis client.

--- src/redis/redisClient.ts
+++ src/redis/redisClient.ts
@@ -1,7 +1,12 @@
+import chalk from "chalk";
 import { createClient } from "redis";
 
 const redisClient = createClient();
 
 await redisClient.connect();
+
+redisClient.on("error", (error) => {
+  console.log(chalk.red(`Redis Error: ${error}`));
+});
 
 export default redisClient;
