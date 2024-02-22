98e9a0b45d32a05e60a1c6fb4d2571f4a3016e6e
chadjaa-sofiane <sofianne012@gmail.com>
2024-02-22 15:43:41 +0100

    Configure discord GatewayIntentBits.

---
+++ src/services/discord.ts
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`logged in as ${client.user?.tag}`);
+});
+
+export default client;
