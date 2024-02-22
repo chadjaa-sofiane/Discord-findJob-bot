013e71c625d1a956066a66334977d4dfd74ab8e1
chadjaa-sofiane <sofianne012@gmail.com>
2024-02-22 15:40:11 +0100

    Create the config file.

---
+++ src/configs/index.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+type EnvConfig = {};
+
+const commonConfigs = {
+  discordToken: Bun.env.DISCORD_TOKEN,
+};
+
+const configsValidation = z.object({
+  discordToken: z.string(),
+});
+
+const developmentConfigs = {};
+const productionConfigs = {};
+const testConfigs = {};
+
+const APP_ENV = process.env.BUN_ENV as "development" | "production" | "test";
+
+const envConfigs: Record<typeof APP_ENV, EnvConfig> = {
+  development: developmentConfigs,
+  production: productionConfigs,
+  test: testConfigs,
+};
+
+const config = {
+  ...commonConfigs,
+  envConfigs: envConfigs[APP_ENV],
+};
+
+configsValidation.parse(config);
+
+export default config;
