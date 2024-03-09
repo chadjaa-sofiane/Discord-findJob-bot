d32b8d4bf1096cb1bf0fe6e3a78f61fdc05f8f08
chadjaa-sofiane <sofianne012@gmail.com>
2024-03-09 23:14:50 +0100

    Implement the analyse functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import "./bot/controllers";
-import createDataAnalyzer from "./services/dataAnalyzer";
 import chalk from "chalk";
+import createDataAnalyzer from "./services/dataAnalyzer";
+import client from "./bot/botClient";
+import config from "./configs";
 
-const DATA_ANALYZER_INTERVAL = 1000 * 60; // every one minute
+const DATA_ANALYZER_INTERVAL = 1000 * 60 * 60; // every one hour
 
 const main = async () => {
   try {
     const disconnect = await createDataAnalyzer(DATA_ANALYZER_INTERVAL);
+    await client.login(config.discordToken);
 
     // event handler triggered when the Node.js process is about to exit.
     process.on("exit", () => {
       console.log(chalk.yellow("Graceful exit initiated. Disconnecting..."));
       disconnect();
     });
+
     // event handler triggered when the user process Ctrl+c.
     process.on("SIGINT", () => {
       console.log(chalk.yellow("Ctrl+C detected. Disconnecting..."));
--- src/services/dataAnalyzer.ts
+++ src/services/dataAnalyzer.ts
@@ -1,21 +1,71 @@
 import puppeteer from "puppeteer";
-import sources from "../sources";
+import sources, { Application } from "../sources";
+import redisClient from "../redis/redisClient";
+import { JOB_APPLICATIONS_SET } from "../configs/constant";
+import chalk from "chalk";
+import createAssistant from "./chatGpt";
+import { wait } from "../lib/utils";
+
+const hasBeenProcessed = async (jobApplicationId: string) => {
+  const isMember = await redisClient.sIsMember(
+    JOB_APPLICATIONS_SET,
+    jobApplicationId,
+  );
+  return isMember;
+};
+
+const markAsProcessed = async (jobApplicationId: string) => {
+  await redisClient.sAdd(JOB_APPLICATIONS_SET, jobApplicationId);
+};
 
 const createDataAnalyzer = async (intervalTime: number) => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
-  const fetchApplications = async () => {
-    const apps = Promise.all(
+  const assistant = await createAssistant();
+
+  const fetchProvidersData = async () => {
+    const providersData = Promise.all(
       sources.map(async (sourceProvider) => await sourceProvider(browser)),
     );
-    return apps;
+    return providersData;
+  };
+
+  const analyApplication = async (app: Application) => {
+    try {
+      console.log(`app is currently being processed: ${app.id} ...`);
+
+      const assistantResponse = await assistant.executeSystemInstructions(
+        `Process the following job application: ${JSON.stringify(app)}`,
+      );
+
+      console.log(assistantResponse);
+
+      markAsProcessed(app.id);
+
+      // in case of using the free tier of GPT API you should wait between each proccess.
+      await wait(1000 * 60);
+    } catch (error) {
+      console.log(
+        chalk.redBright(`on error occucred while proccessing ${app.id}`, error),
+      );
+    }
   };
 
   const startAnalys = async () => {
-    const apps = await fetchApplications();
-    console.log(apps);
+    const providersData = await fetchProvidersData();
+
+    for (const data of providersData) {
+      console.log(
+        chalk.yellow(`applications are loading from ${data.name} ...`),
+      );
+      for (const app of data.apps) {
+        if (!app.execlude && !(await hasBeenProcessed(app.id))) {
+          await analyApplication(app);
+        }
+      }
+    }
   };
 
   // runs once at least
