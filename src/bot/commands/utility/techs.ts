220bdc5db751447b1e07b4a5f2657449f5eb261d
chadjaa-sofiane <sofianne012@gmail.com>
2024-03-25 23:22:12 +0100

    cretae a file that retrieve all the techs of users.

---
+++ src/bot/commands/utility/techs.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import { Command } from "./ping";
+import { getAllUserSettings } from "../../../services/chatGpt";
+
+const techsCommand: Command = {
+  data: new SlashCommandBuilder()
+    .setName("techs")
+    .setDescription("List the technologies each user have"),
+  async execute(interaction: CommandInteraction) {
+    const usersSettings = getAllUserSettings();
+    await interaction.reply(
+      JSON.stringify(
+        Object.fromEntries(
+          Object.entries(usersSettings).map(([key, value]) => [
+            key,
+            value.technologies,
+          ]),
+        ),
+      ),
+    );
+  },
+};
+
+export default techsCommand;
